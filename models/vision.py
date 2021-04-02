570ef6e54541a33ea0f1e7350d5c8365972468fb
Yilun Jin <jyl9707@hotmail.com>
2021-04-02 13:26:16 +0800

    Update vision.py, support customized activation.

--- models/vision.py
+++ models/vision.py
@@ -15,16 +15,21 @@ def weights_init(m):
         m.bias.data.uniform_(-0.5, 0.5)
         
 class LeNet(nn.Module):
-    def __init__(self, input_dim, out_dim):
+    def __init__(self, input_dim, out_dim, act = 'relu'):
         super(LeNet, self).__init__()
-        act = nn.Sigmoid
+        if act == 'sigmoid':
+            self.act = nn.Sigmoid
+        elif act == 'tanh':
+            self.act = nn.Tanh
+        elif act == 'relu':
+            self.act = nn.ReLU
         self.body = nn.Sequential(
             nn.Conv2d(input_dim, 12, kernel_size=5, padding=5//2, stride=2),
-            act(),
+            self.act(),
             nn.Conv2d(12, 12, kernel_size=5, padding=5//2, stride=2),
-            act(),
+            self.act(),
             nn.Conv2d(12, 12, kernel_size=5, padding=5//2, stride=1),
-            act(),
+            self.act(),
         )
         self.fc = nn.Sequential(
             nn.Linear(768, out_dim)
@@ -55,13 +60,18 @@ import torch.nn.functional as F
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, act = 'relu'):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-
+        if act == 'relu':
+            self.act = nn.ReLU()
+        elif act == 'tanh':
+            self.act = nn.Tanh()
+        elif act == 'sigmoid':
+            self.act = nn.Sigmoid()
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
@@ -71,18 +81,18 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         # out = F.relu(self.bn1(self.conv1(x)))
-        out = torch.sigmoid(self.bn1(self.conv1(x)))
+        out = self.act(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
         # out = F.relu(out)
-        out = torch.sigmoid(out)
+        out = self.act(out)
         return out
 
 
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, act='relu'):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -90,6 +100,12 @@ class Bottleneck(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        if act == 'relu':
+            self.act = nn.RelU()
+        elif act == 'tanh':
+            self.act = nn.Tanh()
+        elif act == 'sigmoid':
+            self.act = nn.Sigmoid()
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -99,41 +115,47 @@ class Bottleneck(nn.Module):
             )
 
     def forward(self, x):
-        out = torch.sigmoid(self.bn1(self.conv1(x)))
-        out = torch.sigmoid(self.bn2(self.conv2(out)))
+        out = self.act(self.bn1(self.conv1(x)))
+        out = self.act(self.bn2(self.conv2(out)))
         # out = F.relu(self.bn1(self.conv1(x)))
         # out = F.relu(self.bn2(self.conv2(out)))
         out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
         # out = torch.sigmoid(out)
-        out = F.relu(out)
+        out = self.act(out)
         return out
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10, in_channels = 3):
+    def __init__(self, block, num_blocks, num_classes=10, in_channels = 3, act = 'relu'):
         super(ResNet, self).__init__()
         self.in_planes = 64
+        if act == 'relu':
+            self.act = nn.ReLU()
+        elif act == 'tanh':
+            self.act = nn.Tanh()
+        elif act == 'sigmoid':
+            self.act = nn.Sigmoid()
 
         self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=1)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=1)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=1)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, act=act)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=1, act=act)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=1, act=act)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=1, act=act)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride, act):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, act))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
         # out = F.relu(self.bn1(self.conv1(x)))
-        out = torch.sigmoid(self.bn1(self.conv1(x)))
+        out = self.act(self.bn1(self.conv1(x)))
         # print(out.shape)
         out = self.layer1(out)
         # print(out.shape)
@@ -151,11 +173,11 @@ class ResNet(nn.Module):
         return out
 
 
-def ResNet18(nclass = 10, in_channels = 3):
-    return ResNet(BasicBlock, [2,2,2,2], num_classes=nclass, in_channels=in_channels)
+def ResNet18(nclass = 10, in_channels = 3, act = 'relu'):
+    return ResNet(BasicBlock, [2,2,2,2], num_classes=nclass, in_channels=in_channels, act=act)
 
-def ResNet34(nclass = 10, in_channels = 3):
-    return ResNet(BasicBlock, [3,4,6,3], num_classes=nclass, in_channels=in_channels)
+def ResNet34(nclass = 10, in_channels = 3, act = 'relu'):
+    return ResNet(BasicBlock, [3,4,6,3], num_classes=nclass, in_channels=in_channels, act=act)
 
 def ResNet50():
     return ResNet(Bottleneck, [3,4,6,3])
